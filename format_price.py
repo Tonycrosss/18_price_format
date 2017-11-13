2bbb9a0d31dc03ec7b815b465c07e25f29d0ccba
Старовойтов Антон Станиславович <AStarovoytov@alfabank.ru>
2017-11-13 18:32:08 +0500

    убрал лишнее + изменил форматирование

--- format_price.py
+++ format_price.py
@@ -1,12 +1,25 @@
+import sys
+
+
 def format_price(price):
     if isinstance(price, float):
         int_number = int(price)
         pretty_number ='{0:,}'.format(int_number).replace(',', ' ')
+        return pretty_number
+    else:
+        try:
+            int_number = int(price)
+            print(int_number)
+            pretty_number = '{0:,}'.format(int_number).replace(',', ' ')
+        except IndexError:
+            print('error')
     return pretty_number
 
 
 def main():
-    format_price('3334.2442')
+    price_to_format = sys.argv[0]
+    print(price_to_format)
+    print(format_price(price_to_format))
 
 
 if __name__ == '__main__':
