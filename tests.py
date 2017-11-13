626bdff89f3758a7b1c297677fc9762372ab5f23
Старовойтов Антон Станиславович <AStarovoytov@alfabank.ru>
2017-11-13 13:44:17 +0500

    добавил заглушки будущих тестов

--- tests.py
+++ tests.py
@@ -1,1 +1,21 @@
 # TODO place your tests here
+import unittest
+from format_price import format_price
+
+
+class FormatPriceTestCase(unittest.TestCase):
+    def test_string_number(self):
+        pass
+
+    def test_negative_numbers(self):
+        pass
+
+    def test_words(self):
+        pass
+
+    def test_empty_string(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
